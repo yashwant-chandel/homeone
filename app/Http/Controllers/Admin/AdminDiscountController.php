445b31df15f1e5b9416f191f23dd83c52cc80be1
yashwant-chandel <yashwantchandel19@gmail.com>
2023-09-11 11:53:35 +0530

    updates

--- app/Http/Controllers/Admin/AdminDiscountController.php
+++ app/Http/Controllers/Admin/AdminDiscountController.php
@@ -82,4 +82,34 @@ class AdminDiscountController extends Controller
         $discount->update();
         return response()->json(['success'=>'Successfully updated status']);
     }
+
+    public function checkDiscount(Request $request){
+        if ($request->discount_code && $request->amount) {
+            $discount = Discount::where('discount_code', $request->discount_code)
+                ->where('expire_on', '>', now())
+                ->where('status', 1)
+                ->first();
+        
+            if ($discount) {
+                $amount = $request->amount;
+        
+                if ($discount->discount_type === 'fixed') {
+                    $amount -= $discount->amount;
+                } elseif ($discount->discount_type === 'percentage') {
+                    $percentage = $discount->amount;
+                    $amount *= (1 - ($percentage / 100));
+                }
+                if($amount < 0){
+                    $amount = 0;
+                }
+                $amount = number_format((float)$amount, 2, '.', '');
+                return response()->json(['success' => $amount]);
+            }
+        
+            return response()->json(['error' => 'Invalid discount code or expired']);
+        }
+        
+        return response()->json(['error' => 'Invalid request']);
+        
+    }
 }
